877458940d6d9754987004872fdb53a07307b3d7
Roudaina-Jbeli <jbeliroudaina@gmail.com>
2023-11-23 18:02:16 +0100

    test

--- main.js
+++ main.js
@@ -1,70 +1,22 @@
-var navbar=document.querySelector('.header.navbar')
-var menu=document.querySelector('#menu')
+// Your custom JavaScript code can go here
+// You can use this file to add interactivity to your webpage
+// For example, handling user interactions, making AJAX requests, etc.
 
-menu.onclick =() =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-};
+// Toggle mobile menu
+document.getElementById('menu').addEventListener('click', function () {
+    var navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('active');
+});
 
-window.onscroll =() =>{
-menu.classList.remove('fa-times');
-navbar.classList.remove('active')
-};
-
-
-$(document).ready (function() {
-    $('.buttons') .click(function(){
-    $(this).addClass('active').siblings ().removeClass('active');
-    var filter = $(this). attr('data-filter')
-    if(filter == 'all') {
-    $('.diet .box').show (400);
-    }
-    else{
-    $('.diet .box').not('.'+ filter).hide (200);
-    $('.diet .box').filter('.'+ filter).show(400);
-    }
-    });
-    });
-    var swiper =new Syiper('.review-slider',{
-loop: true,
-grabCursor : true ,
-spaceCetveen ,
-breakpoints:{
-0:{
-    slidesPerview:1,
-},
-640:{
-    slidesPerview:2
-},
-768:{
-    slidesPerview:3
-}
-}
-})
-
-
-    $(document).ready(function () {
-        $("#searchButton").click(function () {
-            var searchTerm = $("#searchInput").val().toLowerCase();
-
-            $(".content").each(function () {
-                var contentText = $(this).text().toLowerCase();
-                if (contentText.includes(searchTerm)) {
-                    $(this).show();
-                } else {
-                    $(this).hide();
-                }
-            });
-        });
-    });
-
-
-    // Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// Initialize Swiper for the review section
+var swiper = new Swiper('.review-slider', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
