c7dc93c677d1b2c76e8f5ceaa843a0a74bc9270b
guelmamihiba <guelmamihiba1@gmail.com>
2023-11-23 03:55:38 +0100

    tesst

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+function showLogin() {
+  document.getElementById('login-form').style.display = 'block';
+  document.getElementById('signup-form').style.display = 'none';
+}
 
+function showSignup() {
+  document.getElementById('login-form').style.display = 'none';
+  document.getElementById('signup-form').style.display = 'block';
+}
+
+function loginUser() {
+  alert('Login logic goes here!');
+  return false; 
+}
+
+function signupUser() {
+  alert('Signup logic goes here!');
+  return false; 
+}
 // Get the modal
 var navlink = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+if (event.target == modal) {
+  modal.style.display = "none";
+}
 }
 
 
